a14d4fdbe591df2defd15fe79e44d00c9e4bd6a2
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-13 11:48:14 -0400

    mock code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 // write your code here
+const url = 'http://localhost:3000/ramens';
+const ramenMenu = document.getElementById('ramen-menu');
+
+async function getRamen() {
+    let response = await fetch(url)
+    let json = await response.json();
+    return json;
+    
+}
+
+function displayImages(imgs){
+    imgs.forEach(el => {
+        let newImg = document.createElement('img');
+        newImg.addEventListener('click', (e) => {
+            console.log('display me')
+        })
+        newImg.src = el;
+        ramenMenu.append(newImg);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    getRamen().then(data => {
+        let imgs = []
+        ramen = data
+        console.log(ramen)
+        data.forEach((el, i) => {
+            imgs.push(el.image)
+        })
+        console.log(imgs);
+        displayImages(imgs)
+    })
+
+})
+
+
